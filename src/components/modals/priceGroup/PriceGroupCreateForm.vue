e67f565b5790fce345a5ee435b43fb9f61905170
developer-naimur <contact.naim24@gmail.com>
2026-01-26 15:56:07 +0600

    working on product module

---
+++ src/components/modals/priceGroup/PriceGroupCreateForm.vue
@@ -0,0 +1,107 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import axiosInstance from '@/axiosInstance'
+import { AxiosError } from 'axios'
+import { useMessageStore } from '@/stores/useMessageStore'
+
+const messageStore = useMessageStore()
+
+const emit = defineEmits(['saved', 'cancel'])
+
+/* =====================================================
+   Add Row (same as your page)
+===================================================== */
+const newRows = ref([{ name: '', status: '' }])
+const addRowField = () => newRows.value.push({ name: '', status: '' })
+
+const copyRowField = (index) => {
+  const rowToCopy = { ...newRows.value[index] }
+  newRows.value.splice(index + 1, 0, rowToCopy)
+}
+
+const removeRowField = (index) => {
+  if (newRows.value.length > 1) {
+    newRows.value.splice(index, 1)
+  }
+}
+
+/* =====================================================
+   Submit
+===================================================== */
+const processing = ref(false)
+
+const submitRows = async () => {
+  if (processing.value) return
+  processing.value = true
+
+  try {
+    await axiosInstance.post('/price-groups', {
+      rows: newRows.value
+    })
+
+    messageStore.showSuccess('Price Group created successfully!')
+    emit('saved') // 🔥 IMPORTANT
+    newRows.value = [{ name: '', status: '' }]
+
+  } catch (err) {
+    if (err instanceof AxiosError) {
+      messageStore.showError(err.response?.data?.message || 'Error')
+    } else {
+      messageStore.showError('Unexpected error')
+    }
+  } finally {
+    processing.value = false
+  }
+}
+</script>
+<template>
+  <form @submit.prevent="submitRows" class="space-y-4">
+
+    <div v-for="(row, index) in newRows" :key="index"
+         class="flex gap-4 items-end border-b pb-4">
+
+      <div class="flex-1">
+        <label class="text-sm font-medium">Name *</label>
+        <input v-model="row.name" class="w-full border p-3" />
+      </div>
+
+      <div class="w-40">
+        <label class="text-sm font-medium">Status *</label>
+        <select v-model="row.status" class="w-full border p-3">
+          <option value="">Select</option>
+          <option value="active">Active</option>
+          <option value="inactive">Inactive</option>
+        </select>
+      </div>
+
+      <div class="flex gap-2">
+        <button type="button" @click="removeRowField(index)"
+                :disabled="newRows.length === 1"
+                class="px-3 py-2 bg-red-100">✕</button>
+
+        <button v-if="index === newRows.length - 1"
+                type="button"
+                @click="copyRowField(index)"
+                class="px-3 py-2 bg-blue-500 text-white">⧉</button>
+
+        <button v-if="index === newRows.length - 1"
+                type="button"
+                @click="addRowField"
+                class="px-3 py-2 bg-green-500 text-white">＋</button>
+      </div>
+    </div>
+
+    <div class="flex justify-end gap-2 pt-4">
+      <button type="button" @click="$emit('cancel')" class="px-4 py-2 bg-gray-200">
+        Cancel
+      </button>
+
+      <button type="submit"
+              :disabled="processing"
+              class="px-4 py-2 bg-gray-600 text-white">
+        {{ processing ? 'Saving...' : 'Save Price Group' }}
+      </button>
+    </div>
+
+  </form>
+</template>
--- src/views/Product/Edit.vue
+++ src/views/Product/Edit.vue
@@ -4,7 +4,7 @@ import { useRoute, useRouter } from 'vue-router'
 import axiosInstance from '@/axiosInstance'
 import { AxiosError } from 'axios'
 
-import FormSkeleton from '@/components/Skeleton/Form-1.vue'
+import FormSkeleton from '@/components/Skeleton/Form-2.vue'
 import Breadcrumb from '@/demoDesign/Breadcrumb.vue'
 import ProductMenu from '@/components/inc/SubSidebar/ProductMenu.vue'
 import { useMessageStore } from '@/stores/useMessageStore'
@@ -217,6 +217,44 @@ const isPriceGroupDisabled = (priceGroupId, currentPriceGroupId) => {
 }
 
 
+
+// instant parents create
+import BrandCreateForm from '@/components/modals/brand/BrandCreateForm.vue'
+const showBrandModal = ref(false)
+const onBrandSaved = async () => {
+  showBrandModal.value = false
+  await loadBrands() // dropdown refresh
+}
+
+import CategoryCreateForm from '@/components/modals/category/CategoryCreateForm.vue'
+const showCategoryModal = ref(false)
+const onCategorySaved = async () => {
+  showCategoryModal.value = false
+  await loadCategories() // dropdown refresh
+}
+
+import TaxCreateForm from '@/components/modals/tax/TaxCreateForm.vue'
+const showTaxModal = ref(false)
+const onTaxSaved = async () => {
+  showTaxModal.value = false
+  await loadTaxes() // dropdown refresh
+}
+
+import PriceGroupCreateForm from '@/components/modals/priceGroup/PriceGroupCreateForm.vue'
+const showPriceGroupModal = ref(false)
+const onPriceGroupSaved = async () => {
+  showPriceGroupModal.value = false
+  await loadPriceGroup() // dropdown refresh
+}
+
+import UnitCreateForm from '@/components/modals/unit/UnitCreateForm.vue'
+const showUnitModal = ref(false)
+const onUnitSaved = async () => {
+  showUnitModal.value = false
+  await loadUnits() // dropdown refresh
+}
+
+
 onMounted(() => {
   loadTaxes()
   loadBrands()
@@ -262,81 +300,167 @@ onMounted(() => {
 
         <!-- Row 1 -->
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-          <input v-model="row.name" placeholder="Name *" class="border p-3" />
-          <input v-model="row.sku" placeholder="SKU *" class="border p-3" />
 
-          <select
-            v-model="row.brand_id"
-            class="border p-3"
-            :disabled="brandLoading || !brands.length"
-          >
-            <option value="">Brand *</option>
-            <option v-for="brand in brands" :key="brand.id" :value="brand.id">
-              {{ brand.brand_name }}
-            </option>
-          </select>
+        <div class="flex-1">
+          <label class="block text-gray-600 font-medium mb-1 text-sm">
+            Name <span class="text-red-600">*</span>
+          </label>
+          <input v-model="row.name" placeholder="Name *" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition" />
+        </div>
+        <div class="flex-1">
+          <label class="block text-gray-600 font-medium mb-1 text-sm">
+            SKU <span class="text-red-600">*</span>
+          </label>
+          <input v-model="row.sku" placeholder="SKU *" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition" />
+        </div>
 
-          <select
-            v-model="row.category_id"
-            class="border p-3"
-            :disabled="categoryLoading || !categories.length"
-          >
-            <option value="">Category</option>
-            <option v-for="category in categories" :key="category.id" :value="category.id">
-              {{ category.category_name }}
-            </option>
-          </select>
+        <div class="flex-1">
+          <label class="block text-gray-600 font-medium mb-1 text-sm">
+            Brand <span class="text-red-600">*</span>
+          </label>
+
+          <div class="flex gap-2">
+            <select
+              v-model="row.brand_id"
+              class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition"
+              :disabled="brandLoading || !brands.length"
+            >
+              <option value="">Brand *</option>
+              <option v-for="brand in brands" :key="brand.id" :value="brand.id">
+                {{ brand.brand_name }}
+              </option>
+            </select>
+            <button type="button"
+                    @click="showBrandModal = true"
+                    class="px-3 bg-blue-500 text-white rounded cursor-pointer">
+              +
+            </button>
+          </div>
+
+        </div>
           
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Category
+            </label>
+
+              <div class="flex gap-2">
+                <select
+                  v-model="row.category_id"
+                  class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition"
+                  :disabled="categoryLoading || !categories.length"
+                >
+                  <option value="">Category</option>
+                  <option v-for="category in categories" :key="category.id" :value="category.id">
+                    {{ category.category_name }}
+                  </option>
+                </select>
+                <button type="button"
+                        @click="showCategoryModal = true"
+                        class="px-3 bg-blue-500 text-white rounded cursor-pointer">
+                  +
+                </button>
+              </div>
+
+          </div>
+
         </div>
 
         <!-- Row 2 -->
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
-          <input type="number" v-model="row.cost_price" placeholder="Cost Price" class="border p-3" />
 
-          <input type="number" v-model="row.selling_price" placeholder="Sold Price" class="border p-3" />
-          <select
-              v-model="row.tax_id"
-              class="border p-3"
-              :disabled="taxLoading || !taxes.length"
-            >
-              <option value="">VAT</option>
-              <option v-for="tax in taxes" :key="tax.id" :value="tax.id">
-                {{ tax.tax_name }}
-              </option>
-            </select>
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Cost Price
+            </label>
+            <input type="number" v-model="row.cost_price" placeholder="Cost Price" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition" />
+          </div>
 
-          <input type="number" v-model="row.discount_value" placeholder="Discount" class="border p-3" />
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Sold Price
+            </label>
+           <input type="number" v-model="row.selling_price" placeholder="Sold Price" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition" />
+          </div>
 
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              TAX
+            </label>
 
+            <div class="flex gap-2">
+              <select
+                  v-model="row.tax_id"
+                  class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition"
+                  :disabled="taxLoading || !taxes.length"
+                >
+                  <option value="">TAX</option>
+                  <option v-for="tax in taxes" :key="tax.id" :value="tax.id">
+                    {{ tax.tax_name }}
+                  </option>
+                </select>
+                <button type="button"
+                        @click="showTaxModal = true"
+                        class="px-3 bg-blue-500 text-white rounded cursor-pointer">
+                  +
+                </button>
+              </div>
+            </div>
+
+            <div class="flex-1">
+              <label class="block text-gray-600 font-medium mb-1 text-sm">
+                Discount
+              </label>
+              <input type="number" v-model="row.discount_value" placeholder="Discount" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition" />
+            </div>
         </div>
 
         <!-- Row 3 -->
         <div class="grid grid-cols-1 md:grid-cols-4 gap-4">
+          
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Discount Type
+            </label>
+            <select v-model="row.discount_type" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition">
+              <option value="">Select</option>
+              <option value="flat">Flat</option>
+              <option value="percent">Percent</option>
+            </select>
+          </div>
 
-          <select v-model="row.discount_type" class="border p-3">
-            <option value="">Select</option>
-            <option value="flat">Flat</option>
-            <option value="percent">Percent</option>
-          </select>
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Description
+            </label>
+            <textarea
+              v-model="row.description"
+              placeholder="Description"
+              rows="1"
+              class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition"></textarea>
+            </div>
 
-          <!-- Description -->
-          <textarea
-            v-model="row.description"
-            placeholder="Description"
-            rows="1"
-            class="border p-3"></textarea>
+          <div class="flex-1">
+            <label class="block text-gray-600 font-medium mb-1 text-sm">
+              Status <span class="text-red-600">*</span>
+            </label>
+            <select v-model="row.status" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition">
+              <option value="">Status *</option>
+              <option value="active">Active</option>
+              <option value="inactive">Inactive</option>
+            </select>
+          </div>
 
-          <select v-model="row.status" class="border p-3">
-            <option value="">Status *</option>
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
-          </select>
-
-          <select v-model="row.is_stock_tracked" class="border p-3">
-            <option value="">Stock Tracked? *</option>
-            <option :value="1">Yes</option>
-            <option :value="0">No</option>
-          </select>
+          <div class="flex-1">
+          <label class="block text-gray-600 font-medium mb-1 text-sm">
+            Stock Tracked? <span class="text-red-600">*</span>
+          </label>
+            <select v-model="row.is_stock_tracked" class="w-full border border-gray-300 p-3 focus:ring-2 focus:ring-gray-500 focus:outline-none transition">
+              <option value="">Stock Tracked? *</option>
+              <option :value="1">Yes</option>
+              <option :value="0">No</option>
+            </select>
+          </div>
 
         </div>
 
@@ -356,21 +480,29 @@ onMounted(() => {
               :key="ucIndex"
               class="grid grid-cols-1 md:grid-cols-3 gap-4 items-center"
             >
-              <select
-                v-model="uc.unit_id"
-                class="w-full rounded-lg border-gray-300 px-4 py-2.5 text-sm
-                       focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none"
-              >
-                <option value="">Select Unit *</option>
-                <option
-                  v-for="unit in units"
-                  :key="unit.id"
-                  :value="unit.id"
-                  :disabled="isUnitDisabled(unit.id, uc.unit_id)"
+
+              <div class="flex gap-2">
+                <select
+                  v-model="uc.unit_id"
+                  class="w-full rounded-lg border-gray-300 px-4 py-2.5 text-sm
+                         focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none"
                 >
-                  {{ unit.unit_name }}
-                </option>
-              </select>
+                  <option value="">Select Unit *</option>
+                  <option
+                    v-for="unit in units"
+                    :key="unit.id"
+                    :value="unit.id"
+                    :disabled="isUnitDisabled(unit.id, uc.unit_id)"
+                  >
+                    {{ unit.unit_name }}
+                  </option>
+                </select>
+                <button type="button"
+                        @click="showUnitModal = true"
+                        class="px-3 bg-blue-500 text-white rounded cursor-pointer">
+                  +
+                </button>
+              </div>
 
               <input
                 v-model="uc.multiplier"
@@ -422,21 +554,29 @@ onMounted(() => {
               :key="pgIndex"
               class="grid grid-cols-1 md:grid-cols-3 gap-4 items-center"
             >
-              <select
-                v-model="pg.price_group_id"
-                class="w-full rounded-lg border-gray-300 px-4 py-2.5 text-sm
-                       focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none"
-              >
-                <option value="">Select Price Group</option>
-                <option
-                  v-for="price_group in product_prices"
-                  :key="price_group.id"
-                  :value="price_group.id"
-                  :disabled="isPriceGroupDisabled(price_group.id, pg.price_group_id)"
+
+              <div class="flex gap-2">
+                <select
+                  v-model="pg.price_group_id"
+                  class="w-full rounded-lg border-gray-300 px-4 py-2.5 text-sm
+                         focus:border-blue-500 focus:ring-2 focus:ring-blue-100 outline-none"
                 >
-                  {{ price_group.group_name }}
-                </option>
-              </select>
+                  <option value="">Select Price Group</option>
+                  <option
+                    v-for="price_group in product_prices"
+                    :key="price_group.id"
+                    :value="price_group.id"
+                    :disabled="isPriceGroupDisabled(price_group.id, pg.price_group_id)"
+                  >
+                    {{ price_group.group_name }}
+                  </option>
+                </select>
+                <button type="button"
+                        @click="showPriceGroupModal = true"
+                        class="px-3 bg-blue-500 text-white rounded cursor-pointer">
+                  +
+                </button>
+              </div>
 
               <input
                 v-model="pg.price"
@@ -487,5 +627,73 @@ onMounted(() => {
     </form>
 
   </div>
+
+    <!-- modals -->
+  <!-- brand modal -->
+  <div v-if="showBrandModal"
+     class="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
+
+    <div class="bg-white w-full max-w-xl rounded-xl p-6">
+
+      <h3 class="text-lg font-semibold mb-4">Add New Brand</h3>
+
+      <BrandCreateForm
+        @saved="onBrandSaved"
+        @cancel="showBrandModal = false"
+      />
+
+    </div>
+  </div>
+  <!-- category modal -->
+  <div v-if="showCategoryModal"
+     class="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
+
+    <div class="bg-white w-full max-w-xl rounded-xl p-6">
+
+      <h3 class="text-lg font-semibold mb-4">Add New Category</h3>
+
+      <CategoryCreateForm
+        @saved="onCategorySaved"
+        @cancel="showCategoryModal = false"
+      />
+
+    </div>
+  </div>
+  <!-- Tax modal -->
+  <div v-if="showTaxModal"
+     class="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
+    <div class="bg-white w-full max-w-xl rounded-xl p-6">
+      <h3 class="text-lg font-semibold mb-4">Add New Tax</h3>
+      <TaxCreateForm
+        @saved="onTaxSaved"
+        @cancel="showTaxModal = false"
+      />
+    </div>
+  </div>
+
+  <!-- Price Group modal -->
+  <div v-if="showPriceGroupModal"
+     class="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
+    <div class="bg-white w-full max-w-xl rounded-xl p-6">
+      <h3 class="text-lg font-semibold mb-4">Add New Price Group</h3>
+      <PriceGroupCreateForm
+        @saved="onPriceGroupSaved"
+        @cancel="showPriceGroupModal = false"
+      />
+    </div>
+  </div>
+
+  <!-- Unit modal -->
+  <div v-if="showUnitModal"
+     class="fixed inset-0 bg-black/50 flex items-center justify-center z-100">
+    <div class="bg-white w-full max-w-xl rounded-xl p-6">
+      <h3 class="text-lg font-semibold mb-4">Add New Unit</h3>
+      <UnitCreateForm
+        @saved="onUnitSaved"
+        @cancel="showUnitModal = false"
+      />
+    </div>
+  </div>
+
 </div>
 </template>
